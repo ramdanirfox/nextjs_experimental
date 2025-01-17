7a1d11ad55d2441bc9dfd644471299773ab84e55
Farhan-Syarif-H <farhansyarifhidayatulloh@smkwikrama.sch.id>
2025-01-17 15:38:54 +0700

    create-new-example

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,37 @@
-import type { Config } from "tailwindcss";
+import { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}', // Tentukan folder app untuk Next.js 13+ dengan App Directory
+    './components/**/*.{js,ts,jsx,tsx}', // Jika Anda menggunakan komponen di luar folder app
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
+
+
+
+// import type { Config } from "tailwindcss";
+
+// export default {
+//   content: [
+//     // './pages/**/*.{js,ts,jsx,tsx}',   
+//     // './components/**/*.{js,ts,jsx,tsx}',
+//     // './app/**/*.{js,ts,jsx,tsx}', 
+//     './app/**/*.{js,ts,jsx,tsx}', // Tentukan folder app untuk Next.js 13+ dengan App Directory
+//     './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// } satisfies Config;
